908ecf582f1a455cb7b952e1ccd2008877d4fe40
zander-prinsloo <prinsloo.zander@gmail.com>
2024-10-03 15:26:24 -0400

    func cov weighted lik

--- scripts/define_estimation_functions_3waves_mle_ar1_cov.R
+++ scripts/define_estimation_functions_3waves_mle_ar1_cov.R
@@ -70,3 +70,49 @@ calc_df_probs_cov <- function(df_cov, param_transformed) {
   df_probs
 }
 
+
+
+# input logit transformed parameters (i.e. not probabilities)
+calc_lli_3waves_ar1_cov <- function(df_template, data, param_transformed) {
+  
+  # Note: the theta parameters are expanded into linear functions
+  #         of the beta parameters to include the covariates
+  #         so that:
+  #                  1 - theta1 = beta01 + beta1*Age + beta2*Age^2 + beta3A*Educ
+  #                      theta2 = beta02 + beta1*Age + beta2*Age^2 + beta3B*Educ
+  #         where:
+  #                  logit_inverse(theta1) is job exit rate
+  #                  logit_inverse(theta2) is job entry rate
+  
+  
+  # Make probabilities
+  #param <- logit_inverse(param_transformed)
+  #mu    <- param$theta_2/(param$theta_1 + param$theta_2)
+  
+  df_cov <- joyn::joyn(x          = df_template, 
+                       y          = data, 
+                       by         = c("y1", "y2", "y3"), 
+                       keep       = "left", 
+                       match_type = "1:m", 
+                       reportvar  = FALSE)
+  
+  df_probs <- calc_df_probs_cov(df_cov            = df_cov, 
+                                param_transformed = param_transformed)
+  
+  
+  df_probs <- df_probs_temp |>
+    group_by(y1, y2, y3) |>
+    summarise(joint_p = sum(joint_p), 
+              .groups = "drop")
+  
+  df_lli <- df_estimate |>
+    joyn::left_join(df_probs, 
+                    by = c('y1', 'y2', 'y3')) |>
+    mutate(lli = weight*log(joint_p)) %>%
+    pull(lli)
+  
+  df_lli
+  
+}
+
+
